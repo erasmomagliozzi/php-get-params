3c53a913647149e6ff6f1d1e0f260077019f736a
Erasmo Magliozzi <magliozzierasmo98@gmail.com>
2020-02-21 17:19:08 +0100

    commit

---
+++ snack 3/index.php
@@ -0,0 +1,27 @@
+<!-- Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi <p>. Ogni punto un nuovo paragrafo. -->
+<?php
+  $text = "Le stazioni si somigliano tutte; poco importa se le luci non riescono a rischiarare più in là del loro alone sbavato, tanto questo è un ambiente che tu conosci a memoria, con l’odore di treno che resta anche dopo che tutti i treni sono partiti, l’odore speciale delle stazioni dopo che è partito l’ultimo treno.Le stazioni si somigliano tutte; poco importa se le luci non riescono a rischiarare più in là del loro alone sbavato, tanto questo è un ambiente che tu conosci a memoria, con l’odore di treno che resta anche dopo che tutti i treni sono partiti, l’odore speciale delle stazioni dopo che è partito l’ultimo treno.";
+
+  $paragrafi = explode(". ", $text);
+
+ ?>
+
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title></title>
+   </head>
+   <body>
+     <div>
+       <?php
+        for ($i=0; $i < count($paragrafi) ; $i++) {
+          echo "<p> $paragrafi[$i] </p>";
+        }
+        ?>
+
+     </div>
+
+   </body>
+ </html>
